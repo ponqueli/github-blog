fc02a0cb588a36da24b5f3a6e9dd163dd36b2f01
Jose Antonio Ponqueli Conto <zehconto@gmail.com>
2023-02-25 13:06:57 -0300

    feat: finish default layout component with outlet

--- src/layouts/DefaultLayout/index.tsx
+++ src/layouts/DefaultLayout/index.tsx
@@ -1,3 +1,4 @@
+import { Outlet } from 'react-router-dom'
 import { Header } from '../../components/Header'
 import { LayoutContainer } from './styles'
 
@@ -5,6 +6,7 @@ export function DefaultLayout() {
   return (
     <LayoutContainer>
       <Header />
+      <Outlet />
     </LayoutContainer>
   )
 }
