9861bacb8a586b93a3d22088327ed2ef739d2e2c
Jose Antonio Ponqueli Conto <zehconto@gmail.com>
2023-02-25 19:37:55 -0300

    feat: create github api call

--- src/pages/Home/components/Profile/index.tsx
+++ src/pages/Home/components/Profile/index.tsx
@@ -2,8 +2,19 @@ import { useState } from 'react'
 import { ProfileAvatar, ProfileContainer } from './styles'
 import { Spinner } from '../../../../components/Spinner'
 
+export interface ProfileData {
+  login: string
+  bio: string
+  avatar_url: string
+  name: string
+  html_url: string
+  company?: string
+  followers: number
+}
+
 export function Profile() {
-  const [isLoadingProfileData, setIsLoadingProfileData] = useState(true)
+  const [profileData, setProfileData] = useState<ProfileData>({} as ProfileData)
+  const [isLoadingProfileData, setIsLoadingProfileData] = useState(false)
   return (
     <ProfileContainer>
       {isLoadingProfileData ? (
