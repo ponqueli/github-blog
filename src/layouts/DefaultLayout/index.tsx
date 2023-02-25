74488bfeab5494b1b3eabc1748184fb56eafdec2
Jose Antonio Ponqueli Conto <zehconto@gmail.com>
2023-02-25 12:53:46 -0300

    feat: create header, defaultLayout and first route

---
+++ src/layouts/DefaultLayout/index.tsx
@@ -0,0 +1,10 @@
+import { Header } from '../../components/Header'
+import { LayoutContainer } from './styles'
+
+export function DefaultLayout() {
+  return (
+    <LayoutContainer>
+      <Header />
+    </LayoutContainer>
+  )
+}
---
+++ src/layouts/DefaultLayout/styles.ts
@@ -0,0 +1,12 @@
+import styled from 'styled-components'
+
+export const LayoutContainer = styled.div`
+  .container {
+    width: 100%;
+    max-width: 57rem;
+    margin-top: -5.5rem;
+    margin-right: auto;
+    margin-left: auto;
+    padding: 0 1rem;
+  }
+`
