9d5979181df306b1e40e095a6a1d3a237ee6d62f
Jose Antonio Ponqueli Conto <zehconto@gmail.com>
2023-02-25 19:51:02 -0300

    feat: get data from github api call

--- src/pages/Home/components/Profile/index.tsx
+++ src/pages/Home/components/Profile/index.tsx
@@ -1,6 +1,7 @@
-import { useState } from 'react'
+import { useCallback, useEffect, useState } from 'react'
 import { ProfileAvatar, ProfileContainer } from './styles'
 import { Spinner } from '../../../../components/Spinner'
+import { githubAPI } from '../../../../libs/githubAPI'
 
 export interface ProfileData {
   login: string
@@ -12,16 +13,36 @@ export interface ProfileData {
   followers: number
 }
 
+// get username from .env.example file
+const username = import.meta.env.VITE_GITHUB_USERNAME
+
 export function Profile() {
   const [profileData, setProfileData] = useState<ProfileData>({} as ProfileData)
   const [isLoadingProfileData, setIsLoadingProfileData] = useState(false)
+
+  const fetchProfileData = useCallback(async () => {
+    try {
+      setIsLoadingProfileData(true)
+      const { data } = await githubAPI.get(`/users/${username}`)
+      setProfileData(data)
+    } catch (error) {
+      console.log(error)
+    } finally {
+      setIsLoadingProfileData(false)
+    }
+  }, [])
+
+  useEffect(() => {
+    fetchProfileData()
+  }, [fetchProfileData])
+
   return (
     <ProfileContainer>
       {isLoadingProfileData ? (
         <Spinner />
       ) : (
         <>
-          <ProfileAvatar />
+          <ProfileAvatar src={profileData.avatar_url} alt="profile picture" />
         </>
       )}
     </ProfileContainer>
